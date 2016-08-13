d2e1d535d83a264125ab1c09b464df06e064def0
Николай <nikols93@mail.ru>
2016-08-13 16:22:06 +0300

    добавили классы Вертикальная линия и фигура, ввели понятие наследования

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80,25);
+            HorizontalLine lineUp = new HorizontalLine(0,78,0,'+');            
+            HorizontalLine lineDown = new HorizontalLine(0, 78, 24, '+');            
+            VerticalLine lineLeft = new Snake.VerticalLine(0, 24, 0, '+');            
+            VerticalLine lineRight = new Snake.VerticalLine(0, 24, 78, '+');
+            lineUp.Drow();
+            lineDown.Drow();
+            lineLeft.Drow();
+            lineRight.Drow();
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5,10,8,'+');
-            line.Drow();
 
             Console.ReadKey();
         }
