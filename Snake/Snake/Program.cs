23bdc9697caec5e2b0da3a02ec9038a5a5a0fd93
Николай <nikols93@mail.ru>
2016-08-13 19:50:27 +0300

    добавили класс Змейка и её Направление

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,15 +13,16 @@ namespace Snake
             Console.SetBufferSize(80,25);
             HorizontalLine lineUp = new HorizontalLine(0,78,0,'+');            
             HorizontalLine lineDown = new HorizontalLine(0, 78, 24, '+');            
-            VerticalLine lineLeft = new Snake.VerticalLine(0, 24, 0, '+');            
-            VerticalLine lineRight = new Snake.VerticalLine(0, 24, 78, '+');
+            VerticalLine lineLeft = new VerticalLine(0, 24, 0, '+');            
+            VerticalLine lineRight = new VerticalLine(0, 24, 78, '+');
             lineUp.Drow();
             lineDown.Drow();
             lineLeft.Drow();
             lineRight.Drow();
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadKey();
         }
